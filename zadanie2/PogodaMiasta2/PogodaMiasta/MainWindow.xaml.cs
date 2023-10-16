3d4d3259b0c384de8ef81573dd231e12da2773f3
SzymonRogozinski <pokeszymon.pl@gmail.com>
2023-10-16 12:25:35 +0200

    Dodanie folderu zadanie2

---
+++ zadanie2/PogodaMiasta2/PogodaMiasta/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using P04WeatherForecastAPI.Client.Models;
+using P04WeatherForecastAPI.Client.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P04WeatherForecastAPI.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AccuWeatherService accuWeatherService;
+        public MainWindow()
+        {
+            InitializeComponent();
+            accuWeatherService = new AccuWeatherService();
+        }
+
+        private async void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            //Dodanie migającego loadingu
+            pbSearching.Visibility = Visibility.Visible;
+            City[] cities = await accuWeatherService.GetLocations(txtCity.Text);
+            pbSearching.Visibility = Visibility.Hidden;
+            // standardowy sposób dodawania elementów
+            //lbData.Items.Clear();
+            //foreach (var c in cities)
+            //    lbData.Items.Add(c.LocalizedName);
+
+            // teraz musimy skorzystac z bindowania danych bo chcemy w naszej kontrolce przechowywac takze id miasta 
+            lbData.ItemsSource = cities;
+        }
+
+        private async void lbData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCity = (City)lbData.SelectedItem;
+            if (selectedCity != null)
+            {
+                txtCity.Text =selectedCity.LocalizedName;
+                //Wyświetlenie migającego loadingu
+                pbWaitingForSearchedTown.Visibility = Visibility.Visible;
+                var weather = await accuWeatherService.GetCurrentConditions(selectedCity.Key);
+                lblCityName.Content = selectedCity.LocalizedName;
+                double tempValue = weather.Temperature.Metric.Value;
+                lblTemperatureValue.Content = Convert.ToString(tempValue);
+                //Wyświetlenie migającego loadingu
+                pbWaitingForSearchedTown.Visibility = Visibility.Hidden;
+            }
+        }
+
+        //Wyświetla sąsiednie miasta
+        private async void btnLoadTown_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedCity = (City)lbData.SelectedItem;
+            if (selectedCity != null)
+            {
+                //Wyświetlenie migającego loadingu
+                pbWaitingForTowns.Visibility = Visibility.Visible;
+                string[] city_list=await accuWeatherService.GetNeighbourCities(selectedCity.Key);
+                lvNeighbouringCities.ItemsSource=city_list;
+                //Wyświetlenie migającego loadingu
+                pbWaitingForTowns.Visibility = Visibility.Hidden;
+            }
+        }
+
+        //Wyświetla więcej informacji
+        private async void btnMoreInfo_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedCity = (City)lbData.SelectedItem;
+            if (selectedCity != null)
+            {
+                //Wyświetlenie migającego loadingu
+                pbWaitingForDeatils.Visibility = Visibility.Visible;
+                string[] weather_list = await accuWeatherService.GetDetailWeather(selectedCity.Key);
+                lblDayWth.Content= weather_list[0];
+                lblNightWth.Content = weather_list[1];
+                var curr= await accuWeatherService.GetCurrentWeather(selectedCity.Key);
+                lblCurrentWth.Content = curr;
+                //Wyświetlenie migającego loadingu
+                pbWaitingForDeatils.Visibility = Visibility.Hidden;
+            }
+        }
+
+        //Wyświetla alert
+        private async void btnCheckAlert_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedCity = (City)lbData.SelectedItem;
+            if (selectedCity != null)
+            {
+                //Wyświetlenie migającego loadingu
+                pbWaitingForAlerts.Visibility = Visibility.Visible;
+                string[] alert = await accuWeatherService.GetWeatherAlert(selectedCity.Key);
+                if (alert==null)
+                {
+                    lblAlertStatus.Content = "No alert";
+                }
+                else 
+                {
+                    lblAlertStatus.Content = "Alert!";
+                    lblAlertDate.Content = alert[0];
+                    lblAlertType.Content = alert[1];
+                    lblAlertValue.Content = alert[2];
+                }
+                //Wyświetlenie migającego loadingu
+                pbWaitingForAlerts.Visibility = Visibility.Hidden;
+            }
+        }
+
+        //Wyświetla lokalizację na bazie ip
+        private async void btnCheckIP_Click(object sender, RoutedEventArgs e)
+        {
+
+            //Wyświetlenie migającego loadingu
+            pbWaitingForIP.Visibility = Visibility.Visible;
+            string ip_string = await accuWeatherService.GetIPLocation();
+            lblIPLocation.Content = ip_string;
+            //Wyświetlenie migającego loadingu
+            pbWaitingForIP.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ zadanie2/PogodaMiasta2/PogodaMiasta/Services/AccuWeatherService.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using P04WeatherForecastAPI.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace P04WeatherForecastAPI.Client.Services
+{
+    internal class AccuWeatherService
+    {
+        private const string base_url = "http://dataservice.accuweather.com";
+        private const string autocomplete_endpoint = "locations/v1/cities/autocomplete?apikey={0}&q={1}&language{2}";
+        private const string current_conditions_endpoint = "currentconditions/v1/{0}?apikey={1}&language{2}";
+
+        private const string neibour_cities_endpoint = "locations/v1/cities/neighbors/{0}?apikey={1}%20&language={2}&details=false";
+        private const string det_weather_endpoint = "forecasts/v1/daily/1day/{0}?apikey={1}%20&language={2}&metric=true";
+        private const string current_weather_endpoint = "currentconditions/v1/{0}?apikey={1}&language={2}";
+        private const string alarms_weather_endpoint = "alarms/v1/1day/{0}?apikey={1}&language={2}";
+        private const string ip_location_endpoint = "locations/v1/cities/ipaddress?apikey={0}&q={1}&language{2}";
+
+
+        private const string api_key = "BXREHEO1Lz6rPy3ff7V2S53bLbIASv8o";
+        //string api_key;
+        //private const string language = "pl";
+        string language;
+
+        public AccuWeatherService()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<App>()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetings.json"); 
+
+            var configuration = builder.Build();
+            //api_key = configuration["api_key"];
+            language = configuration["default_language"];
+        }
+
+
+        public async Task<City[]> GetLocations(string locationName)
+        {
+            string uri = base_url + "/" + string.Format(autocomplete_endpoint, api_key, locationName, language);
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(uri);
+                string json = await response.Content.ReadAsStringAsync();
+                City[] cities = JsonConvert.DeserializeObject<City[]>(json);
+                return cities;
+
+            }
+        }
+
+        public async Task<Weather> GetCurrentConditions(string cityKey)
+        {
+            string uri = base_url + "/" + string.Format(current_conditions_endpoint, cityKey, api_key,language);
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(uri);
+                string json = await response.Content.ReadAsStringAsync();
+                Weather[] weathers= JsonConvert.DeserializeObject<Weather[]>(json);
+                return weathers.FirstOrDefault();
+            }
+        }
+
+        public async Task<string[]> GetNeighbourCities(string cityKey) 
+        {
+            string uri = base_url + "/" + string.Format(neibour_cities_endpoint, cityKey, api_key, language);
+            using (HttpClient client = new HttpClient())
+            {
+                //Wczytanie danych
+                var response = await client.GetAsync(uri);
+                string json = await response.Content.ReadAsStringAsync();
+                //Odpakowanie ich
+                dynamic cities = JsonConvert.DeserializeObject<dynamic>(json);
+                List<string> cityList=new List<string>();
+                //Wczytanie pojedyńczego obiektu z listy
+                foreach (dynamic city in cities) 
+                {
+                    //Dodanie string z nazwą miasta
+                    cityList.Add(city["LocalizedName"].ToString());
+                }
+                return cityList.ToArray();
+            }
+        }
+
+        public async Task<string[]> GetDetailWeather(string cityKey)
+        {
+            string uri = base_url + "/" + string.Format(det_weather_endpoint, cityKey, api_key, language);
+            using (HttpClient client = new HttpClient())
+            {
+                //Wczytanie danych
+                var response = await client.GetAsync(uri);
+                string json = await response.Content.ReadAsStringAsync();
+                //Odpakowanie ich
+                dynamic weather = JsonConvert.DeserializeObject<dynamic>(json);
+                List<string> weathers = new List<string>();
+                dynamic v1 = weather["DailyForecasts"][0].Day.IconPhrase.ToString();
+                weathers.Add(v1);
+                v1 = weather["DailyForecasts"][0].Night.IconPhrase.ToString();
+                weathers.Add(v1);
+                return weathers.ToArray();
+            }
+        }
+
+        public async Task<string> GetCurrentWeather(string cityKey)
+        {
+            string uri = base_url + "/" + string.Format(current_weather_endpoint, cityKey, api_key, language);
+            using (HttpClient client = new HttpClient())
+            {
+                //Wczytanie danych
+                var response = await client.GetAsync(uri);
+                string json = await response.Content.ReadAsStringAsync();
+                //Odpakowanie ich
+                dynamic Curr_weather = JsonConvert.DeserializeObject<dynamic>(json);
+                string weather = Curr_weather[0]["WeatherText"].ToString();
+                return weather;
+            }
+        }
+
+        public async Task<string[]> GetWeatherAlert(string cityKey)
+        {
+            string uri = base_url + "/" + string.Format(alarms_weather_endpoint, cityKey, api_key, language);
+            using (HttpClient client = new HttpClient())
+            {
+                //Wczytanie danych
+                var response = await client.GetAsync(uri);
+                string json = await response.Content.ReadAsStringAsync();
+                if (json=="[]")
+                    return null;
+                //Odpakowanie ich
+                dynamic alerts = JsonConvert.DeserializeObject<dynamic>(json);
+                
+
+                List<string> alarm = new List<string>();
+                string v1 = alerts["Date"].ToString();
+                alarm.Add(v1);
+                v1 = alerts["Alarms"].AlarmType.ToString();
+                alarm.Add(v1);
+                v1 = alerts["Alarms"].Value.Metric.Value.ToString()+ alerts["Alarms"].Value.Metric.Unit.ToString();
+                alarm.Add(v1);
+                return alarm.ToArray();
+            }
+        }
+
+        public async Task<string> GetIPLocation()
+        {
+            //Pobranie ip użytkownika
+            var externalIp = await GetExternalIpAddress();
+            externalIp = externalIp ?? IPAddress.Loopback;
+
+            string uri = base_url + "/" + string.Format(ip_location_endpoint, api_key, externalIp, language);
+            using (HttpClient client = new HttpClient())
+            {
+                //Wczytanie danych
+                var response = await client.GetAsync(uri);
+                string json = await response.Content.ReadAsStringAsync();
+                //Odpakowanie ich
+                dynamic Ip_data = JsonConvert.DeserializeObject<dynamic>(json);
+                string weather = Ip_data["LocalizedName"].ToString();
+                return weather;
+            }
+        }
+
+        private static async Task<IPAddress?> GetExternalIpAddress()
+        {
+            var externalIpString = (await new HttpClient().GetStringAsync("http://icanhazip.com"))
+                .Replace("\\r\\n", "").Replace("\\n", "").Trim();
+            if (!IPAddress.TryParse(externalIpString, out var ipAddress)) return null;
+            return ipAddress;
+        }
+    }
+}
