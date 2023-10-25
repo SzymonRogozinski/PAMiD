d3aadabdcf041018a7bd784ed22280b71d4ddfff
SzymonRogozinski <pokeszymon.pl@gmail.com>
2023-10-25 21:05:48 +0200

    Inicjalizacja zadania 3 i 4

---
+++ zadanie3/L3/P06Shop.Shared/Library/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P06Shop.Shared.Library
+{
+    public class Book
+    {
+        public string name { get; set; }
+        public string author { get; set; }
+        public int pages { get; set; }
+        public List<Genre> genres { get; set; }
+    }
+
+    public class Genre
+    {
+        public string name { get; set; }
+    }
+}
---
+++ zadanie3/L3/P07Shop.DataSeeder/BookSeeder.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using P06Shop.Shared.Library;
+using P06Shop.Shared.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P07Shop.DataSeeder
+{
+    public class BookSeeder
+    {
+
+        public static List<Book> GenerateProductData()
+        {
+            int productId = 1;
+            var productFaker = new Faker<Book>()
+                .RuleFor(x => x.name, f => f.Lorem.Word() + " " + f.Lorem.Word())
+                .RuleFor(x => x.author, f => f.Name.FullName())
+                .RuleFor(x => x.pages, f => f.Random.Int(50, 2000))
+                .RuleFor(x => x.genres, f => GenerateGenres());
+
+            return productFaker.Generate(10).ToList();
+
+        }
+
+        private static List<Genre> GenerateGenres ()
+        { 
+            return null; 
+        }
+    }
+}
